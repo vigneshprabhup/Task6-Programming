035890be5d9f77db34fc9273206c0102ccd16b80
vigneshprabhup <68711174+vigneshprabhup@users.noreply.github.com>
2020-08-09 23:23:53 -0400

    Programming
    Create a class named 'Programming'. While creating an object of the class, if nothing is passed to it, then the message "I love programming languages" should be printed. If some String is passed to it, then in place of "programming languages" the name of that String variable should be printed. 
    
    For example, while creating object if we pass "Java", then "I love Java" should be printed. There can be more than one programming languages.

---
+++ MainProgramming.java
@@ -0,0 +1,15 @@
+package BankImplementation;
+import java.util.*;
+public class MainProgramming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Programming obj1=new Programming();
+		
+		Programming obj2=new Programming("Java");
+		Programming obj3=new Programming("C+=");
+		Programming obj4=new Programming("C#");
+	}
+
+}
---
+++ Programming.java
@@ -0,0 +1,15 @@
+package BankImplementation;
+
+public class Programming {
+
+	
+	public Programming()
+	{
+		System.out.println("I love Programming");
+	}
+	
+	public Programming(String str)
+	{
+		System.out.println("I Love" + str);
+	}
+}
